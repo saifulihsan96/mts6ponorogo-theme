ff17cfb39cf0de88f4cfd2464e929e606d05dd60
Saiful Ihsan <37842597+saifulihsan96@users.noreply.github.com>
2024-01-28 11:55:21 +0700

    finish header

--- header.php
+++ header.php
@@ -63,12 +63,18 @@
 
 								<div class="mobile-menu">
 
-									<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
+									<div class="icon_bar">
+										<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
 										<path d="M10.5 19C10.5 19.2761 10.7239 19.5 11 19.5L20 19.5C20.2761 19.5 20.5 19.2761 20.5 19L20.5 17C20.5 16.7239 20.2761 16.5 20 16.5L11 16.5C10.7239 16.5 10.5 16.7239 10.5 17L10.5 19ZM6.5 3C6.5 3.27614 6.72386 3.5 7 3.5L20 3.5C20.2761 3.5 20.5 3.27614 20.5 3L20.5 0.999999C20.5 0.723857 20.2761 0.499999 20 0.499999L7 0.5C6.72386 0.5 6.5 0.723858 6.5 1L6.5 3ZM0.5 11C0.5 11.2761 0.723857 11.5 1 11.5L20 11.5C20.2761 11.5 20.5 11.2761 20.5 11L20.5 9C20.5 8.72386 20.2761 8.5 20 8.5L1 8.5C0.723857 8.5 0.5 8.72386 0.5 9L0.5 11Z" fill="black" stroke="black" stroke-linejoin="round"/>
-									</svg>
+										</svg>
+									</div>
 
 									<div class="setmenu">
-										<div class="close-menu">close</div>
+										<div class="close-menu">
+											<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+											<path d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z" fill="black"/>
+											</svg>
+										</div>
 										<?php
 										wp_nav_menu( [
 											'menu'       => 'Menu 1',
--- js/script.js
+++ js/script.js
@@ -10,3 +10,30 @@ document.addEventListener( 'click', function(event) {
         search_form.classList.remove( 'active' );
     }
 });
+
+const item_mobile_menu = document.querySelectorAll( '.mobile-menu .main-mobile-header li' );
+for (let i = 0; i < item_mobile_menu.length; i++) {
+    const item_menu = item_mobile_menu[i];
+    item_menu.addEventListener( 'click', function() {
+        const sub_menu = this.querySelector( '.sub-menu' );
+        sub_menu.classList.toggle( 'active' );
+    });
+}
+
+const bar_menu     = document.querySelector( '.mobile-menu .icon_bar svg' );
+const overlay_menu = document.querySelector( '.mobile-menu .overlay-menu' );
+const close_menu   = document.querySelector( '.mobile-menu .close-menu svg' );
+
+bar_menu.addEventListener( 'click', function() {
+    const menu_mobile = this.closest( '.mobile-menu' );
+    const setmenu     = menu_mobile.querySelector( '.setmenu' );
+    setmenu.classList.add( 'active_menu' );
+    overlay_menu.classList.add( 'active' );
+});
+
+close_menu.addEventListener( 'click', function() {
+    const menu_mobile = this.closest( '.mobile-menu' );
+    const setmenu     = menu_mobile.querySelector( '.setmenu' );
+    setmenu.classList.remove( 'active_menu' );
+    overlay_menu.classList.remove( 'active' );
+});
