03fe6ede91392d218c67caff6504d1e103a1a988
saiful ihsan <saifulihsan1996@gmail.com>
2025-05-28 12:00:19 +0700

    updated install script

--- app-passed/head.php
+++ app-passed/head.php
@@ -14,7 +14,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="stylesheet" id="main-css" href="<?php echo FTR_URI ?>/dist/css/main-passed.css">
+    <link rel="stylesheet" id="main-css" href="<?php echo FTR_URI ?>/dist/css/main-passed.css?v=<?php echo filemtime( get_template_directory() . '/dist/css/main-passed.css' ); ?>">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 		<script src="https://unpkg.com/lottie-web@5.12.0/build/player/lottie.min.js"></script>
 		<?php wp_head(); ?>
